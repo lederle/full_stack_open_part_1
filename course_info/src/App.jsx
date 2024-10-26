02438da324950565ce5205d01b823d15b113c7f5
mike lederle <mrlederle@gmail.com>
2024-10-26 16:36:08 -0400

    Total component refactor

--- course_info/src/App.jsx
+++ course_info/src/App.jsx
@@ -14,6 +14,12 @@ const Content = (props) => {
   )
 }
 
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development';
   const part1 = 'Fundamentals of React';
@@ -29,7 +35,7 @@ const App = () => {
       <Content part1={part1} exercises1={exercises1}
                part2={part2} exercises2={exercises2}
                part3={part3} exercises3={exercises3} />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
